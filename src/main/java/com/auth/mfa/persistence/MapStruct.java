99b4d7ea3c35b13896fdc96c468b3d74ab21804b
GadelhaTI <gadelha.ti@gmail.com>
2024-11-16 10:05:08 -0300

    refactor: cleaning up

--- src/main/java/com/auth/mfa/persistence/MapStruct.java
+++ src/main/java/com/auth/mfa/persistence/MapStruct.java
@@ -1,11 +1,7 @@
 package com.auth.mfa.persistence;
 
 import com.auth.mfa.persistence.model.*;
-import com.auth.mfa.persistence.payload.request.DTORequestPrivilege;
-import com.auth.mfa.persistence.payload.request.DTORequestRole;
 import com.auth.mfa.persistence.payload.request.DTORequestUser;
-import com.auth.mfa.persistence.payload.response.DTOResponsePrivilege;
-import com.auth.mfa.persistence.payload.response.DTOResponseRole;
 import com.auth.mfa.persistence.payload.response.DTOResponseUser;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -14,11 +10,7 @@ import org.mapstruct.factory.Mappers;
 public interface MapStruct {
 
     MapStruct MAPPER = Mappers.getMapper(MapStruct.class);
-    DTOResponsePrivilege toDTO(Privilege privilege);
-    DTOResponseRole toDTO(Role role);
     DTOResponseUser toDTO(User user);
 
-    Privilege toObject(DTORequestPrivilege dtoRequestPrivilege);
-    Role toObject(DTORequestRole dtoRequestRole);
     User toObject(DTORequestUser dtoRequestUser);
 }
