d57d72465b2d0455619fa809e0fe932fcf8d54bc
GadelhaTI <gadelha.ti@gmail.com>
2024-11-15 21:02:35 -0300

    chore: define constant application.cors

--- src/main/java/com/auth/mfa/configuration/ConfigurationCors.java
+++ src/main/java/com/auth/mfa/configuration/ConfigurationCors.java
@@ -1,6 +1,8 @@
 package com.auth.mfa.configuration;
 
 import lombok.NonNull;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,13 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ConfigurationCors {
 
+    @Value("${application.public.path}")
+    private String path;
     @Bean
     public WebMvcConfigurer corsConfigure() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(ArrayUtils.addAll(path.split(",")))
                         .allowedMethods("POST", "GET", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true)
