1c5af5656a5813681f6f619534950fdcbbf766c1
GadelhaTI <gadelha.ti@gmail.com>
2024-11-21 01:38:41 -0300

    fix: bean definition message with thymeleaf

--- src/main/java/com/auth/mfa/configuration/ConfigurationMessages.java
+++ src/main/java/com/auth/mfa/configuration/ConfigurationMessages.java
@@ -1,17 +1,20 @@
 package com.auth.mfa.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
-public class ConfigurationThymeleaf {
+public class ConfigurationMessages {
+
     @Bean
     @Description("Spring Message Resolver")
-    public ResourceBundleMessageSource messageSourceThymeleaf() {
+    public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
+        messageSource.setBasename("ValidationMessages");
+        messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
 }
