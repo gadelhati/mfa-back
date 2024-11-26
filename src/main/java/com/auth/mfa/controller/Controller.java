fd648a259dcb4b87d06fb9d73844bdbb38167cf1
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-11-26 16:13:57 -0300

    refactor: thymeleaf pages

--- src/main/java/com/auth/mfa/controller/Controller.java
+++ src/main/java/com/auth/mfa/controller/Controller.java
@@ -1,35 +1,15 @@
 package com.auth.mfa.controller;
 
-import com.auth.mfa.persistence.model.User;
-import com.auth.mfa.persistence.payload.response.DTOResponseUser;
-import com.auth.mfa.persistence.repository.RepositoryUser;
-import com.auth.mfa.service.ServiceUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.awt.print.Pageable;
-import java.util.Optional;
 
 @RestController @RequiredArgsConstructor
 public class Controller {
 
-    private final ServiceUser serviceUser;
-    private final RepositoryUser repositoryUser;
-
-    @GetMapping("/")
-    public String home(Model model, Authentication authentication) {
-        if(authentication != null) {
-            Optional<User> dtoRequestUser = repositoryUser.findByUsername("00038059");
-            model.addAttribute("user", dtoRequestUser);
-        }
-        model.addAttribute("title", "Home");
-        return "index";
-    }
     @GetMapping("/register")
     public ModelAndView sigNup(Model model) {
         ModelAndView modelAndView = new ModelAndView("register");
@@ -37,23 +17,21 @@ public class Controller {
         return modelAndView;
     }
     @GetMapping("/login")
-    public ModelAndView signIn(Model model) {
+    public ModelAndView login(Model model) {
         ModelAndView modelAndView = new ModelAndView("login");
         modelAndView.addObject("title", "Login");
         return modelAndView;
     }
-    @GetMapping("/error")
-    public String error(Model model) throws Throwable {
-        model.addAttribute("title", "Login");
-        model.addAttribute("error", true);
-        return "login";
+    @GetMapping("/logout")
+    public ModelAndView logout(Model model) {
+        ModelAndView modelAndView = new ModelAndView("logout");
+        modelAndView.addObject("title", "Logout");
+        return modelAndView;
     }
-
-    @GetMapping("/users")
-    public String getUsers(Model model) throws Throwable {
-//        DTOResponseUser dtoResponseUser = (DTOResponseUser) serviceUser.retrieve(PageRequest.of(0, 25), ""));
-        model.addAttribute("title", "Login");
-        model.addAttribute("error", true);
-        return "login";
+    @GetMapping("/error")
+    public ModelAndView error(Model model) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("title", "Error");
+        return modelAndView;
     }
 }
